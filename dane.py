f11c841bb2b25c74c8c1cc2c78fe69451c12e1cc
PrecloweKotki <149382336+PrecloweKotki@users.noreply.github.com>
2023-11-06 09:23:40 +0100

    test added data
    Added a new user for test purposes

--- dane.py
+++ dane.py
@@ -3,7 +3,7 @@ num = random.randrange(1,50)
 USER_LIST: list = \
 [{"name":"Scholastyka", "nickname":"Cheesegrater","posts": random.randrange(1,80)},
 {"name":"Edwin","nickname":"CoolDude1222","posts": random.randrange(1,80)},
-{"name":"Kevin","nickname":"Głodniak","posts":random.randrange(1,40)},
+{"name":"Kevin","nickname":"MałyGlod","posts":random.randrange(1,40)},
 {"name":"Stefan","nickname":"Czekoladka","posts":random.randrange(1,55)},
 {"name":"Dio","nickname":"Za Warudo","posts":random.randrange(1,11)},
 {"name":"Janusz","nickname":"Miodek888","posts":random.randrange(1,33)},
@@ -12,5 +12,6 @@ USER_LIST: list = \
 {"name":"Pangea","nickname":"MiałamDinozaury","posts":random.randrange(1,50)},
 {"name":"Xnopyt","nickname":"Franciszek","posts":random.randrange(1,110)},
 {"name":"XPC-313","nickname":"TakMnieNazwano","posts":random.randrange(1,530)},
-{"name":"Luigi", "nickname":"Spaghetti_Carbonara", "posts": random.randrange(1,300)}]
+{"name":"Luigi", "nickname":"Spaghetti_Carbonara", "posts": random.randrange(1,300)},
+{"name":"Mario", "nickname":"Spaghetti_Bolognese", "posts": random.randrange(1,22)}]
 
