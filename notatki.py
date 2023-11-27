38b58d2830e841809a8292d82f2df6341c2c38bc
PrecloweKotki <149382336+PrecloweKotki@users.noreply.github.com>
2023-11-27 13:55:01 +0100

    chore: generowanie mapy

--- notatki.py
+++ notatki.py
@@ -1,3 +1,4 @@
+import folium
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -5,6 +6,7 @@ import re
 # Pobranie strony
 
 town_name = input("STATE THE NAME OF TOWN TO SEARCH")
+
 def get_coordinates(city:str)->list[float,float]:
  URL_adress = (f'https://pl.wikipedia.org/wiki/{city}')
  response = requests.get(url=URL_adress)
@@ -16,3 +18,13 @@ def get_coordinates(city:str)->list[float,float]:
  response_html_longitude = float(response_html_longitude.replace(',','.'))
  return [response_html_latitude,response_html_longitude]
 print(get_coordinates(town_name))
+
+###Rysowanie mapy
+town = get_coordinates(town_name)
+map = folium.Map(location = town,tiles= "OpenStreetMap",zoom_start=11)
+folium.Marker(
+ location= town,
+ popup=[town_name,"GEOINFORMATYKA"],
+ icon= folium.Icon(color="purple")
+).add_to(map)
+map.save('mapa.html')
