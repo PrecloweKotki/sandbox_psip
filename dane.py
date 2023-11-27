6159819d871ae80e97eec92bfd580ff32245d5f5
PrecloweKotki <149382336+PrecloweKotki@users.noreply.github.com>
2023-11-27 15:14:27 +0100

    s

--- dane.py
+++ dane.py
@@ -1,18 +1,18 @@
 import random
 num = random.randrange(1,50)
 USER_LIST: list = \
-[{"name":"Scholastyka", "nickname":"Cheesegrater","posts": random.randrange(1,80)},
-{"name":"Edwin","nickname":"CoolDude1222","posts": random.randrange(1,80)},
-{"name":"Kevin","nickname":"MałyGlod","posts":random.randrange(1,40)},
-{"name":"Stefan","nickname":"Czekoladka","posts":random.randrange(1,55)},
-{"name":"Dio","nickname":"Za Warudo","posts":random.randrange(1,11)},
-{"name":"Janusz","nickname":"Miodek888","posts":random.randrange(1,33)},
-{"name":"Alfred","nickname":"WójekAlfred","posts":random.randrange(1,50)},
-{"name":"Ilona","nickname":"Skowronka_Parzzona","posts": random.randrange(1,50)},
-{"name":"Pangea","nickname":"MiałamDinozaury","posts":random.randrange(1,50)},
-{"name":"Xnopyt","nickname":"Franciszek","posts":random.randrange(1,110)},
-{"name":"XPC-313","nickname":"TakMnieNazwano","posts":random.randrange(1,530)},
-{"name":"Luigi", "nickname":"Spaghetti_Carbonara", "posts": random.randrange(1,300)},
-{"name":"Mario", "nickname":"Spaghetti_Bolognese", "posts": random.randrange(1,22)},
-{"name":"Mario", "nickname":"Spaghetti_Napoli", "posts": random.randrange(1,43)}]
+[{"name":"Scholastyka", "nickname":"Cheesegrater","posts": random.randrange(1,80),"town_name":"Istebna"},
+{"name":"Edwin","nickname":"CoolDude1222","posts": random.randrange(1,80),"town_name":"Sompolno"},
+{"name":"Kevin","nickname":"MałyGlod","posts":random.randrange(1,40),"town_name":"Kotor"},
+{"name":"Stefan","nickname":"Czekoladka","posts":random.randrange(1,55),"town_name":"Dubrownik"},
+{"name":"Dio","nickname":"Za Warudo","posts":random.randrange(1,11),"town_name":"Kair"},
+{"name":"Janusz","nickname":"Miodek888","posts":random.randrange(1,33),"town_name":"Kioto"},
+{"name":"Alfred","nickname":"WójekAlfred","posts":random.randrange(1,50),"town_name":"Wrocław"},
+{"name":"Ilona","nickname":"Skowronka_Parzzona","posts": random.randrange(1,50),"town_name":"Nuuk"},
+{"name":"Pangea","nickname":"MiałamDinozaury","posts":random.randrange(1,50),"town_name":"Kielce"},
+{"name":"Xnopyt","nickname":"Franciszek","posts":random.randrange(1,110),"town_name":"Alert"},
+{"name":"XPC-313","nickname":"TakMnieNazwano","posts":random.randrange(1,530),"town_name":"Londyn"},
+{"name":"Luigi", "nickname":"Spaghetti_Carbonara", "posts": random.randrange(1,300),"town_name":"Rzym"},
+{"name":"Mario", "nickname":"Spaghetti_Bolognese", "posts": random.randrange(1,22),"town_name":"Wenecja"},
+{"name":"Mario", "nickname":"Spaghetti_Napoli", "posts": random.randrange(1,43),"town_name":"Neapol"}]
 
--- notatki.py
+++ notatki.py
@@ -1,14 +1,11 @@
 import folium
 from bs4 import BeautifulSoup
 import requests
-import re
+from dane import USER_LIST
 
 # Pobranie strony
-
-town_name = input("STATE THE NAME OF TOWN TO SEARCH")
-
-def get_coordinates(city:str)->list[float,float]:
- URL_adress = (f'https://pl.wikipedia.org/wiki/{city}')
+def get_coordinates(town_name:str)->list[float,float]:
+ URL_adress = (f'https://pl.wikipedia.org/wiki/{town_name}')
  response = requests.get(url=URL_adress)
  response_html = BeautifulSoup(response.text, 'html.parser')
 # Pobranie współrzędnych
@@ -17,14 +14,18 @@ def get_coordinates(city:str)->list[float,float]:
  response_html_longitude = response_html.select('.longitude')[1].text
  response_html_longitude = float(response_html_longitude.replace(',','.'))
  return [response_html_latitude,response_html_longitude]
-print(get_coordinates(town_name))
+print(get_coordinates('town_name'))
+def get_map_of(user:str)->None:
+ map = folium.Map(location=[52.3,21.0], tiles="OpenStreetMap", zoom_start=4)
+ for user in USER_LIST:
+  folium.Marker(
+   location=get_coordinates(city = user["town_name"]),
+   popup= f'"USER: {user["name"]}\n'
+          f'"POST NUMBER:{user["posts"]}\n',
+   icon=folium.Icon(color="purple")
+  ).add_to(map)
+ map.save('mapa.html')
+
+
 
 ###Rysowanie mapy
-town = get_coordinates(town_name)
-map = folium.Map(location = town,tiles= "OpenStreetMap",zoom_start=11)
-folium.Marker(
- location= town,
- popup=[town_name,"GEOINFORMATYKA"],
- icon= folium.Icon(color="purple")
-).add_to(map)
-map.save('mapa.html')
